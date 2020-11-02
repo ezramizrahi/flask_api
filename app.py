9d96e1b7596981962daf6348652d21072870618b
Ezra Mizrahi <32469862+ezramizrahi@users.noreply.github.com>
2020-11-02 17:43:06 +1100

    files

---
+++ app.py
@@ -0,0 +1,45 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+
+# data
+data = [
+    {'name': 'Ezra Mizrahi',
+     'title': 'software engineer',
+     'skills': [
+     'javascript',
+     'react.js',
+     'node.js',
+     'python',
+     'ruby',
+     'golang',
+     'e2e testing',
+     'cypress.io',
+     'selenium',
+     'test driven development',
+     'jest',
+     'enzyme',
+     'rspec',
+     'CI/CD',
+     'AWS',
+     'bash scripting'
+     ],
+     'email': 'ezra.mizrahi@hey.com',
+     'blog_url': 'https://gitcommit.netlify.app/',
+     'github_url': 'https://github.com/ezramizrahi'}
+]
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Ezra Mizrahi</h1>
+<p>About Me</p>'''
+
+# returns all info about Ezra Mizrahi
+@app.route('/api/v1/resources/ezra/all', methods=['GET'])
+def api_all():
+    return jsonify(data)
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000, debug=True, use_reloader=True)
